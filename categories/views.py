db62969359589781c9cc278b5fad57639580c2f9
choosooa <josuah5160@dgu.ac.kr>
2026-06-23 03:45:48 +0900

    current 백엔드 구현

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.db.models import ProtectedError
+
 from .models import Category
+from items.models import Item
 
 def create(request):
     if not request.user.is_authenticated:
@@ -16,7 +18,7 @@ def create(request):
         new_category.name = request.POST['name']
         new_category.save()
 
-        return redirect('items:storage')  # 수정 필요
+        return redirect('categories:current')  # 수정 필요
         
 def delete(request, category_id):
     if not request.user.is_authenticated:
@@ -26,16 +28,60 @@ def delete(request, category_id):
 
     # 기본 카테고리 삭제 불가
     if delete_category.is_default:
-        return redirect('items:storage')  # 수정 필요
+        return redirect('categories:current')  # 수정 필요
 
     # 본인 카테고리 아니면 막음
     if delete_category.creator != request.user:
-        return redirect('items:storage')  # 수정 필요
+        return redirect('categories:current')  # 수정 필요
 
     # 아이템 있으면 삭제 불가
     try:
         delete_category.delete()
     except ProtectedError:
-        return redirect('items:storage')  # 수정 필요
+        return redirect('categories:current')  # 수정 필요
 
-    return redirect('items:storage')  # 수정 필요
+    return redirect('categories:current')  # 수정 필요
+
+#current 함수 추가 
+def current(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:login')
+    
+    #닉네임
+    nickname = request.user.profile.nickname
+    #전체 아이템 개수 
+    total_items_count = Item.objects.filter(owner_user=request.user, is_deleted=False).count()
+    #기본 카테고리 
+    default_categories = Category.objects.filter(is_default=True)
+    #내가 만든 카테고리 
+    my_categories = Category.objects.filter(creator=request.user)
+    
+    categories = []
+
+    # 기본 카테고리 별 개수 세기 
+    for category in default_categories:
+        category.item_count = Item.objects.filter(
+            owner_user=request.user, category=category, is_deleted=False).count()
+
+        categories.append(category)
+
+    # 내가 만든 카테고리별 개수 세기
+    for category in my_categories:
+        category.item_count = Item.objects.filter(
+            owner_user=request.user,
+            category=category,
+            is_deleted=False
+        ).count()
+
+        categories.append(category)
+
+    # 편집 모드인지 확인
+    is_edit = False
+
+    if request.GET.get('edit') == '1':
+        is_edit = True 
+
+    return render(request, 'categories/current.html', {
+        'nickname': nickname, 'total_items_count': total_items_count,
+        'categories': categories, 'is_edit': is_edit })
+    
